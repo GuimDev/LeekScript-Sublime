507ab040c9661396af13f9b7d088b81587203203
GuimDev <GuimDev@users.noreply.github.com>
2017-06-03 17:07:47 +0200

    Fix #1 and limit to Linux & Windows

--- LeekScript.py
+++ LeekScript.py
@@ -4,9 +4,11 @@ import sublime, sublime_plugin, platform, os
 
 class LeekloudDownloadCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		if (platform.system() == "Linux"):
+		if (platform.platform() == "linux"):
 			os.system("gnome-terminal -e \"node '"+sublime.packages_path()+"/LeekScript Syntax/nodeLKlauncher.js'\" &")
-		else:
+		else if (platform.platform() == "windows"):
 			os.system("start node \""+sublime.packages_path()+"/LeekScript Syntax/nodeLKlauncher.js\" &")
+		else:
+			sublime.status_message("Only Windows & Linux (gnome-terminal). Renseignez la commande dans LeekScript.py.")
 
 		sublime.status_message("Execution de LKDownloader...")
